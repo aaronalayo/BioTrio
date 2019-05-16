83bf8dbed74dff5f00b5fd1100443bc651c53eeb
Maria <maria_ilieva28@abv.bg>
2019-05-16 14:17:33 +0200

    Auto stash before merge of "master" and "screen"

---
+++ src/main/java/dk/kea/dat18i/team8/biotrio/demo/Seat/SeatGroup.java
@@ -0,0 +1,9 @@
+package dk.kea.dat18i.team8.biotrio.demo.Seat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeatGroup {
+
+    private List<Seat> seats=new ArrayList<>();
+}
