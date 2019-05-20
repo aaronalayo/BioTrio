45e2cf50e34b357985a60dfa90aa3ad27bd29a5a
aAron <aaro0186@stud.kea.dk>
2019-05-20 15:30:51 +0200

    screening tostring

--- src/main/java/dk/kea/dat18i/team8/biotrio/demo/screenings/ScreeningForm.java
+++ src/main/java/dk/kea/dat18i/team8/biotrio/demo/screenings/ScreeningForm.java
@@ -43,9 +43,8 @@ public class ScreeningForm {
     }
 
 
-
     @Override
     public String toString() {
-        return "ScreeningForm{" + "showing='" + showing + '\'' + ", movie_id=" + movie_id + '}';
+        return "ScreeningForm{" + "showing='" + showing + '\'' + ", movie_id=" + movie_id + ", theater_id=" + theater_id + '}';
     }
 }
